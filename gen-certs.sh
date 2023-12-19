91e2210124c5b91c73be7ef47fcca535f1d83803
Evans Mungai <mbuevans@gmail.com>
2023-12-19 11:39:19 +0000

    Add server.crt leaf cert

--- gen-certs.sh
+++ gen-certs.sh
@@ -18,5 +18,6 @@ openssl req -newkey rsa:2048 -nodes -keyout server.key -subj "/CN=server" -out s
 openssl req -newkey rsa:2048 -nodes -keyout client.key -subj "/CN=client" -out client.csr
 openssl x509 -req -extfile <(printf "subjectAltName=DNS:example.com,IP:127.0.0.1") -days 365 -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt
 chmod 600 server.key
+cat server.crt ca.crt > server-fullchain.crt
 openssl req -newkey rsa:2048 -nodes -keyout client.key -subj "/CN=client" -out client.csr
 openssl x509 -req -days 365 -in client.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out client.crt
